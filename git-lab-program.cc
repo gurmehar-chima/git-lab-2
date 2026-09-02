4d5fc553586288a0d3a35e258ec6461684607adf
Gurmehar Chima <gc756325@ohio.edu>
2026-09-02 18:18:12 -0400

    Creating git-lab 2 that says hello

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Gurmehar Chima
+ *        Date: 09/02/2026
+ * Description: Creating a new file for the Git Lab assignment.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
